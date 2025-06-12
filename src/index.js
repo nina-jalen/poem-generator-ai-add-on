68ec2c29f2d7a5055c3210c8f4c2e802a3f3a835
Nina Jalen <nina.jalen1@gmail.com>
2025-06-12 17:06:33 +0200

    Added the API integration

--- src/index.js
+++ src/index.js
@@ -5,13 +5,42 @@ function generatePoem(event) {
 
   //   let poemELement = document.querySelector("#poem");
   //   poemELement.innerHTML = "This is generated poem";
+  // 1
+  //   new Typewriter("#poem", {
+  //     strings: "This is generated poem",
+  //     autoStart: true,
+  //     delay: 1,
+  //     cursor: "",
+  //   });
 
-  new Typewriter("#poem", {
-    strings: "This is generated poem",
-    autoStart: true,
-    delay: 1,
-    cursor: "",
-  });
+  // 2
+  // built the API URL
+  // Make a call to API
+  // Display the generated poem
+
+  function displayPoem(response) {
+    // respose.data.answer
+    console.log("Poem genetared");
+    new Typewriter("#poem", {
+      strings: response.data.answer,
+      autoStart: true,
+      delay: 1,
+      cursor: "",
+    });
+  }
+
+  let instructionsInput = document.querySelector("#user-instructions");
+
+  let apiKey = "7e0645b54e0a0f4c0fa7443bo6fb8tf9";
+  let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+  let context =
+    "You are a romantic poem expert and love to write short poems. Your mission is to generate a short, a 4 line poem in basic HTML (but do not incluede HTML and ` in text, in poem) and separete each line with a break. Sign the poem with SheCodes AI inside an <i> element at the end of the poem and not in the beggining beform the poem in a new line after the poem with a break. Make sure to follow user instructions.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating poem ... ");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
