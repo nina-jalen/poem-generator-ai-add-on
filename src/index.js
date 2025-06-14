597a824b35cdf0a2947affde4790ae274feed911
Nina Jalen <nina.jalen1@gmail.com>
2025-06-14 16:28:45 +0200

    Improved the UX

--- src/index.js
+++ src/index.js
@@ -37,6 +37,12 @@ function generatePoem(event) {
     "You are a romantic poem expert and love to write short poems. Your mission is to generate a short, a 4 line poem in basic HTML (but do not incluede HTML and ` in text, in poem) and separete each line with a break. Sign the poem with SheCodes AI inside an <i> element at the end of the poem and not in the beggining beform the poem in a new line after the poem with a break. Make sure to follow user instructions.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  // 3 - UX
+  // to show the poem element when submiting a word
+  let poemELement = document.querySelector("#poem");
+  poemELement.classList.remove("hidden");
+  poemELement.innerHTML = `<div class="genetaring">Generating a poem for you about ${instructionsInput.value} ... </div>`;
+
   console.log("Generating poem ... ");
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
@@ -45,3 +51,5 @@ function generatePoem(event) {
 
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
+
+// remove all the console.logs after finishing the project !
